a996726ae42c8343480de1fc19d7257731b4499c
jingchaoluan <jingchaoluan@gmail.com>
2017-08-29 19:21:29 -0400

    Added testing time code

--- OCR/OCRopus/call_bin_script.py
+++ OCR/OCRopus/call_bin_script.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ##########################################################################################
-# Developer: Luan,Jingchao        Project: HuMaIN (http://humain.acis.ula.ve)
+# Developer: Luan,Jingchao        Project: HuMaIN (http://humain.acis.ufl.edu)
 # Description: 
 #	Given the original images' directory to variable 'imageDir', the script will call OCR 
 # binarization microservice for each image file in the directory. 
@@ -25,78 +25,69 @@
 import requests, zipfile, StringIO
 import time, argparse, os, sys, subprocess
 
+start_time = time.time()
 
 ### Validation and receive parameters
-def valid_and_receive_args():
-	parser = argparse.ArgumentParser("Call OCRopy Binarization Service")
+parser = argparse.ArgumentParser("Call OCRopy Binarization Service")
 
-	parser.add_argument('image')
-	parser.add_argument('-o','--output',default=None,help="output directory, without the last slash")
-	parser.add_argument('-t','--threshold',type=float,default=0.5,help='threshold, determines lightness, default: %(default)s')
-	parser.add_argument('-z','--zoom',type=float,default=0.5,help='zoom for page background estimation, smaller=faster, default: %(default)s')
-	parser.add_argument('-e','--escale',type=float,default=1.0,help='scale for estimating a mask over the text region, default: %(default)s')
-	parser.add_argument('-b','--bignore',type=float,default=0.1,help='ignore this much of the border for threshold estimation, default: %(default)s')
-	parser.add_argument('-p','--perc',type=float,default=80,help='percentage for filters, default: %(default)s')
-	parser.add_argument('-r','--range',type=int,default=20,help='range for filters, default: %(default)s')
-	parser.add_argument('-m','--maxskew',type=float,default=2,help='skew angle estimation parameters (degrees), default: %(default)s')
-	parser.add_argument('--lo',type=float,default=5,help='percentile for black estimation, default: %(default)s')
-	parser.add_argument('--hi',type=float,default=90,help='percentile for white estimation, default: %(default)s')
-	parser.add_argument('--skewsteps',type=int,default=8,help='steps for skew angle estimation (per degree), default: %(default)s')
+parser.add_argument('image', help="The path of a image file, or a folder containing all pre-process images.")
+parser.add_argument('-o','--output',default=None, help="output directory, without the last slash")
+parser.add_argument('-t','--threshold',type=float, default=argparse.SUPPRESS, help='threshold, determines lightness')
+parser.add_argument('-z','--zoom',type=float,default=argparse.SUPPRESS, help='zoom for page background estimation, smaller=faster')
+parser.add_argument('-e','--escale',type=float,default=argparse.SUPPRESS, help='scale for estimating a mask over the text region')
+parser.add_argument('-b','--bignore',type=float,default=argparse.SUPPRESS, help='ignore this much of the border for threshold estimation')
+parser.add_argument('-p','--perc',type=float,default=argparse.SUPPRESS, help='percentage for filters')
+parser.add_argument('-r','--range',type=int,default=argparse.SUPPRESS, help='range for filters')
+parser.add_argument('-m','--maxskew',type=float,default=argparse.SUPPRESS, help='skew angle estimation parameters (degrees)')
+parser.add_argument('--lo',type=float,default=argparse.SUPPRESS, help='percentile for black estimation')
+parser.add_argument('--hi',type=float,default=argparse.SUPPRESS, help='percentile for white estimation')
+parser.add_argument('--skewsteps',type=int,default=argparse.SUPPRESS, help='steps for skew angle estimation (per degree)')
 
-	args = parser.parse_args()
+args = parser.parse_args()
 
-	### The existence of the source image is verified
-	if not os.path.isfile(args.image):
-		print("Error: File %s was not found" % (imagepath))
-		parser.print_help()
-		sys.exit(0)
-
-	### The existence of the destination folder is verified or created
-	if args.output is not None:
+### The existence of the destination folder is verified or created
+if args.output is None:
+	# If output folder is not set, save output image to current directory
+	args.output = os.getcwd()
+else:
+	if not os.path.isdir(args.output):
+		subprocess.call(["mkdir -p " + args.output], shell=True)
 		if not os.path.isdir(args.output):
-			subprocess.call(["mkdir -p " + args.output], shell=True)
-			if not os.path.isdir(args.output):
-				print("Error: Destination folder %s could not be created" % (args.output))
-				sys.exit(0)
+			print("Error: Destination folder %s could not be created" % (args.output))
+			sys.exit(0)
 
-	return vars(args)
+args = vars(args)
 
 
 def call_bin(imagepath, dstDir, parameters):
-	url_bin = 'http://10.5.146.92:8001/binarizationapi'
+	url_bin = 'http://localhost:8001/binarizationapi'
 
 	# Uploaded iamges
 	image = {'image': open(imagepath, 'rb')}
 
 	# Call binarization service
+	#print("*** Send request: %.4f ***" % time.time())
+	call_begin = time.time()
 	resp = requests.get(url_bin, files=image, data=parameters, stream=True)
+	print("*** Bin service time: %.4f ***" % (time.time()-call_begin))
 
 	# Save the responsed binarized image
 	image = os.path.basename(imagepath)
 	image_name, image_ext = os.path.splitext(image)
 	dstimage = image_name + ".bin.png"
-	dstpath = ""
-	if dstDir is None:
-		# If dst folder is none, save output image to current directory
-		dstpath = os.path.join(os.getcwd(), dstimage)
-	else:
-		dstpath = os.path.join(dstDir, dstimage)
+	dstpath = os.path.join(dstDir, dstimage)
 
 	if resp.status_code == 200:
 		with open(dstpath, 'wb') as fd:
 			for chunk in resp:
 				fd.write(chunk)
 	else:
-		print("\nERROR code: %d" % resp.status_code)
+		print("Image %s Binarization error!" % imagepath)
 		#print(resp.content)
 
 
 if __name__ == '__main__':
-	start_time = time.time()
-	### validation and receive parameters
-	args = valid_and_receive_args()
-
-	imagepath = args['image']
+	image = args['image']
 	dstDir = args['output']
 
 	### Only keep the setable parameters
@@ -104,6 +95,20 @@ if __name__ == '__main__':
 	del args['output']
 
 	### Call binarization service
-	call_bin(imagepath, dstDir, args)
-
-	print("\n--- %s seconds ---" % (time.time() - start_time))
+	if os.path.isfile(image):
+		print("\n===== %s ======" % image)
+		call_bin(image, dstDir, args)
+	elif os.path.isdir(image):
+		for image_name in os.listdir(image):
+			image_path = os.path.join(image, image_name)
+			# One image failed do not affect the process of other images
+			try:
+				print("\n========== %s ==========" % image_name)
+				call_bin(image_path, dstDir, args)
+			except:
+				pass
+	else:
+		parser.print_help()
+		sys.exit(0)
+	
+	print("--- Over all invoke time: %.4f seconds ---" % (time.time() - start_time))
